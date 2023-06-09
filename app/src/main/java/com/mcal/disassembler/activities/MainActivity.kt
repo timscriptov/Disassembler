d0ed319c9516860a7e48931160fb5856ef652d7c
Иван Тимашков <timscriptov@mail.ru>
2023-06-09 13:07:40 +0500

    Update MainActivity.kt

--- app/src/main/java/com/mcal/disassembler/activities/MainActivity.kt
+++ app/src/main/java/com/mcal/disassembler/activities/MainActivity.kt
@@ -6,13 +6,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.mcal.disassembler.R
 import com.mcal.disassembler.adapters.ListAdapter
